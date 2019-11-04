9c5195169be3b92da2c26849b277b368e9a421c8
taeyoung-labtop <taeyounglee93@gmail.com>
2019-11-04 18:21:34 +0900

    11.04

--- data/신입생~1/make_json.py
+++ data/신입생~1/make_json.py
@@ -1,18 +1,19 @@
 import xlrd,os,json
 from pprint import pprint
-file_list = os.listdir('./')
+file_list = os.listdir(r'data\신입생~1')
 file_list.sort()
 # 일반고, 특수목적고, 특성화고, 자율고, 기타 총입학자수.
-
+# filename : 2page_2013.json
 for file_name in file_list:
     if file_name.find('.xls') != -1 and int(file_name[:4]) >= 2013:
         year = int(file_name[:4])
-        file = xlrd.open_workbook(file_name)
+        file = xlrd.open_workbook(r'data\신입생~1\\'+file_name)
         sheet = file.sheet_by_index(0)
         nrow = sheet.nrows
         ppl_by_school_type = {'normal':0, 'special_intent':0,'specified':0,'free':0,'etc':0}
-        for row_num in range(6,nrow-1):
+        for row_num in range(5,nrow-1):
             ppl_by_school_type['normal'] += int(sheet.cell_value(row_num, 6))
+            #print(sheet.cell_value(row_num, 6))
             ppl_by_school_type['special_intent'] += int(sheet.cell_value(row_num, 8))
             ppl_by_school_type['special_intent'] += int(sheet.cell_value(row_num, 10))
             ppl_by_school_type['special_intent'] += int(sheet.cell_value(row_num, 12))
@@ -24,6 +25,6 @@ for file_name in file_list:
             ppl_by_school_type['etc'] += int(sheet.cell_value(row_num, 24))
         print(str(year)+':',end='')
         pprint(ppl_by_school_type)
-        with open(str(year)+'.json','w',encoding='UTF-8-sig') as outfile:
+        with open('data\신입생~1\\2page_'+str(year)+'.json','w',encoding='UTF-8-sig') as outfile:
             outfile.write(json.dumps(ppl_by_school_type,ensure_ascii=False))
     
