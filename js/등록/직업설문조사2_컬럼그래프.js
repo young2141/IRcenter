b1cb7c61a2e2d4d548e98e17d52b77f03db0bb57
dohan kim <sphist94@gmail.com>
2019-12-08 15:48:47 +0900

    컬럼 그래프 추가

---
+++ js/등록/직업설문조사2_컬럼그래프.js
@@ -0,0 +1,71 @@
+function drawChart() {
+    am4core.ready(function () {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        // Create chart instance
+        var chart = am4core.create("chartdiv", am4charts.XYChart);
+
+        // Add data
+        chart.data = [{
+            "Employed Full-Time": 90,
+            "Seeking Employment": 10,
+            "x": "> 6 Months Before Grad"
+        }, {
+            "Employed Full-Time": 75,
+            "Seeking Employment": 25,
+            "x": "3-6 Months Before Grad"
+        }, {
+            "Employed Full-Time": 63,
+            "Seeking Employment": 37,
+            "x": "< 3 Months Before Grad"
+        }];
+
+        // Create axes
+        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "x";
+        categoryAxis.renderer.grid.template.location = 0;
+
+
+        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.inside = true;
+        valueAxis.renderer.labels.template.disabled = true;
+        valueAxis.min = 0;
+        valueAxis.max = 100;
+
+        // Create series
+        function createSeries(field, name, color) {
+
+            // Set up series
+            var series = chart.series.push(new am4charts.ColumnSeries());
+            series.name = name;
+            series.dataFields.valueX = field;
+            series.dataFields.categoryY = "x";
+            series.sequencedInterpolation = true;
+            series.columns.template.fill = color;
+
+            // Make it stacked
+            series.stacked = true;
+
+            // Configure columns
+            series.columns.template.width = am4core.percent(60);
+            series.columns.template.tooltipText = "[bold]{name}[/]\n[font-size:14px]{categoryX}: {valueY}";
+
+            // Add label
+            var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+            labelBullet.label.text = "{valueX}";
+            labelBullet.locationX = 0.5;
+
+            return series;
+        }
+
+        createSeries("Seeking Employment", "Seeking Employment", am4core.color("#FCD12A"));
+        createSeries("Employed Full-Time", "Employed Full-Time", chart.colors.getIndex(1).brighten(0));
+
+        // Legend
+        chart.legend = new am4charts.Legend();
+
+    }); // end am4core.ready()
+}
