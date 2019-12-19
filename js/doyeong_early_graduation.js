1b53aab801713fceda456ca34e87dd8b79bdefe6
dongneong_notebook <akfls12@naver.com>
2019-12-19 17:56:32 +0900

    조기졸업자,복수전공자,부전공자 막대그래프 구현 및 데이터 추가

---
+++ js/doyeong_early_graduation.js
@@ -0,0 +1,85 @@
+function parsing() {
+    var year = parseInt($("#years").val());
+    var filename = "doyeong_early_graduation.json";
+    $.getJSON("../../../json/" + filename, (jsonData) => {
+        var cnt = 0;
+        while (cnt < 5) {
+            var data = [];
+            for (var i = 0; i < jsonData.length; i++) {
+                if (jsonData[i]["category"] == String(year)) {
+                    data = [
+                        {
+                            "category": "조기졸업자",
+                            "value": jsonData[i]["early_graduation"]
+                        },
+                        {
+                            "category": "복수전공자",
+                            "value": jsonData[i]["multiple_major"]
+                        },
+                        {
+                            "category": "부전공자",
+                            "value": jsonData[i]["sub_major"]
+                        }
+                    ];
+                    draw(data, 5 - cnt, year--)
+                    cnt++;
+                    break;
+                }
+            }
+        }
+    })
+}
+
+function draw(_data, _N, _year) {
+    am4core.ready(function () {
+        am4core.useTheme(am4themes_animated);
+
+        console.log(_data, _N);
+        var chart = am4core.create("chartdiv" + String(_N), am4charts.XYChart);
+
+        // Add data
+        chart.data = _data;
+
+        // Create axes
+
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "category";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 30;
+
+        categoryAxis.renderer.labels.template.adapter.add("dy", function (dy, target) {
+            if (target.dataItem && target.dataItem.index & 2 == 2) {
+                return dy + 2;
+            }
+            return dy;
+        });
+
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.min = 0;
+        valueAxis.max = 800;
+
+        // Create series
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueY = "value";
+        series.dataFields.categoryX = "category";
+        series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+        series.columns.template.fillOpacity = .8;
+
+        categoryAxis.renderer.labels.template.adapter.add("dy", function (dy, target) {
+            if (target.dataItem && target.dataItem.index & 2 == 2) {
+                return dy + 25;
+            }
+            return dy;
+        });
+
+        var columnTemplate = series.columns.template;
+        columnTemplate.strokeWidth = 2;
+        columnTemplate.strokeOpacity = 1;
+
+        var title = chart.titles.create();
+        title.fontSize = 15;
+        title.text = String(_year) + "학년도";
+        title.dx = 0;
+        title.dy = 0;
+    });
+}
--- js/doyeong_graduation_status_pie.js
+++ js/doyeong_graduation_status_pie.js
@@ -1,40 +1,39 @@
-function parsing(){
+function parsing() {
     var year = parseInt($('#years').val())// string 타입
     var filename = "doyeong_graduation_status_pie.json";
     $.getJSON("../../../json/" + filename, (jsonData) => {
         var cnt = 0;
         var num = 5;
-        while(cnt < 5){
-            for(var i=0; i<jsonData.length; i++){
-                if(String(year) == jsonData[i]["category"]){
+        while (cnt < 1) {
+            for (var i = 0; i < jsonData.length; i++) {
+                if (String(year) == jsonData[i]["category"]) {
                     var data = [
                         {
-                            "category" : "진학",
-                            "value" : jsonData[i]["enter"]
+                            "category": "진학",
+                            "value": jsonData[i]["enter"]
                         },
                         {
-                            "category" : "취업자",
-                            "value" : jsonData[i]["employment"]
+                            "category": "취업자",
+                            "value": jsonData[i]["employment"]
                         },
                         {
-                            "category" : "입대자",
-                            "value" : jsonData[i]["millitary"]
+                            "category": "입대자",
+                            "value": jsonData[i]["millitary"]
                         },
                         {
-                            "category" : "외국인유학생",
-                            "value" : jsonData[i]["foreign"]
+                            "category": "외국인유학생",
+                            "value": jsonData[i]["foreign"]
                         },
                         {
-                            "category" : "기타/미상",
-                            "value" : jsonData[i]["etc"]
+                            "category": "기타/미상",
+                            "value": jsonData[i]["etc"]
                         },
                         {
-                            "category" : "취업불가능자/제외인정자",
-                            "value" : jsonData[i]["exception"]
+                            "category": "취업불가능자/제외인정자",
+                            "value": jsonData[i]["exception"]
                         }
                     ];
-                    //console.log(jsonData[i])
-                    draw(data, num--, year--);
+                    draw(data);
                     cnt++;
                     break;
                 }
@@ -43,20 +42,19 @@ function parsing(){
     })
 }
 
-function draw(_data, _N, _year){
+function draw(_data) {
     am4core.ready(function () {
         am4core.useTheme(am4themes_animated);
         am4core.useTheme(am4themes_material);
-        var chart = am4core.create("chartdiv" + String(_N), am4charts.PieChart);
+        var chart = am4core.create("chartdiv", am4charts.PieChart);
         chart.data = _data;
-        console.log(_data, _N);
+        console.log(_data);
         var title = chart.titles.create();
-        title.text = _year + "년도"
         title.fontSize = 15;
         title.dx = 140;
         title.dy = 0;
         createSeries(chart);
-      });
+    });
 }
 
 function createSeries(_chart) {
@@ -71,18 +69,18 @@ function createSeries(_chart) {
     //   "[#000]2019학년도 전체 신입생 중 {full_name} 출신은 [bold]{value.percent.formatNumber('#.#')}%[] 입니다.";
     // 툴팁에 실제 value값도 필요하다면 ({value}명) 추가하기
     pieSeries.tooltip.label.fill = am4core.color("#000000");
-    
+
     // This creates initial animation
     pieSeries.hiddenState.properties.opacity = 1;
     pieSeries.hiddenState.properties.endAngle = -90;
     pieSeries.hiddenState.properties.startAngle = -90;
     pieSeries.colors.list = [
-      "#ff3d00",
-      "#ef5350",
-      "#ff7f27",
-      "#ffc102",
-      "#fdf5bc"
+        "#ff3d00",
+        "#ef5350",
+        "#ff7f27",
+        "#ffc102",
+        "#fdf5bc"
     ].map(color => {
-      return new am4core.color(color);
+        return new am4core.color(color);
     });
-  }
+}
