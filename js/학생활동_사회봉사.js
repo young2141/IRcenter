4261353469d50f913b38555626f42c26276aec0b
imjspk <59077504+imjspk@users.noreply.github.com>
2020-01-27 21:16:15 +0900

    [완료] 학생활동_사회봉사

--- js/학생활동_사회봉사.js
+++ js/학생활동_사회봉사.js
@@ -6,7 +6,7 @@ function parse(callback) {
     });
 }
 
-function draw_graph(_year, _type) {
+function draw_graph(_year, _type, isupdate) {
     parse(json => {
         /*
         var jdata = []
@@ -25,72 +25,73 @@ function draw_graph(_year, _type) {
         // ------------------------------------------------------------------------------------------------------------------------------------------------------
         // draw_trend
         // ------------------------------------------------------------------------------------------------------------------------------------------------------
-        am4core.ready(function () {
-            // Themes begin
-            am4core.useTheme(am4themes_animated);
-            // Themes end
 
-            // Create chart instance
-            var chart = am4core.create("chartdiv1", am4charts.XYChart);
-            chart.data = json
-            // chart.data = [{ "year": 2010.0, "농촌봉사": 656.0, "교육": 564.0, "노인": 39.0, "보건의료": 123.0, "문화": 115.0, "사무행정": 44.0, "국제협력": 55.0, "아동청소년": 136.0, "장애인": 225.0, "캠퍼스": 204.0, "환경": 264.0, "전체": 2425.0 }, { "year": 2011.0, "농촌봉사": 840.0, "교육": 489.0, "노인": 34.0, "보건의료": 119.0, "문화": 37.0, "사무행정": 157.0, "국제협력": 98.0, "아동청소년": 52.0, "장애인": 68.0, "캠퍼스": 167.0, "환경": 450.0, "전체": 2511.0 }, { "year": 2012.0, "농촌봉사": 184.0, "교육": 664.0, "노인": 36.0, "보건의료": 228.0, "문화": 129.0, "사무행정": 73.0, "국제협력": 159.0, "아동청소년": 120.0, "장애인": 76.0, "캠퍼스": 1366.0, "환경": 81.0, "전체": 3116.0 }, { "year": 2013.0, "농촌봉사": 929.0, "교육": 585.0, "노인": 68.0, "보건의료": 289.0, "문화": 131.0, "사무행정": 74.0, "국제협력": 221.0, "아동청소년": 174.0, "장애인": 66.0, "캠퍼스": 729.0, "환경": 31.0, "전체": 3297.0 }, { "year": 2014.0, "농촌봉사": 56.0, "교육": 455.0, "노인": 32.0, "보건의료": 173.0, "문화": 114.0, "사무행정": 48.0, "국제협력": 355.0, "아동청소년": 114.0, "장애인": 25.0, "캠퍼스": 361.0, "환경": 37.0, "전체": 1770.0 }, { "year": 2015.0, "농촌봉사": 382.0, "교육": 500.0, "노인": 49.0, "보건의료": 205.0, "문화": 63.0, "사무행정": 67.0, "국제협력": 259.0, "아동청소년": 48.0, "장애인": 35.0, "캠퍼스": 1436.0, "환경": 262.0, "전체": 3306.0 }, { "year": 2016.0, "농촌봉사": 170.0, "교육": 367.0, "노인": 146.0, "보건의료": 252.0, "문화": 129.0, "사무행정": 41.0, "국제협력": 121.0, "아동청소년": 248.0, "장애인": 192.0, "캠퍼스": 432.0, "환경": 129.0, "전체": 2227.0 }, { "year": 2017.0, "농촌봉사": 510.0, "교육": 563.0, "노인": 171.0, "보건의료": 224.0, "문화": 96.0, "사무행정": 30.0, "국제협력": 133.0, "아동청소년": 249.0, "장애인": 92.0, "캠퍼스": 897.0, "환경": 203.0, "전체": 3168.0 }, { "year": 2018.0, "농촌봉사": 206.0, "교육": 478.0, "노인": 97.0, "보건의료": 180.0, "문화": 127.0, "사무행정": 64.0, "국제협력": 183.0, "아동청소년": 342.0, "장애인": 101.0, "캠퍼스": 252.0, "환경": 114.0, "전체": 2144.0 }, { "year": 2019.0, "농촌봉사": 206.0, "교육": 478.0, "노인": 97.0, "보건의료": 180.0, "문화": 127.0, "사무행정": 64.0, "국제협력": 183.0, "아동청소년": 342.0, "장애인": 101.0, "캠퍼스": 252.0, "환경": 114.0, "전체": 2144.0 }]
+        if (isupdate) {
+            am4core.ready(function () {
+                // Themes begin
+                am4core.useTheme(am4themes_animated);
+                // Themes end
 
-            var max_value = 0, min_value = 10000
-            for (var i = 0; i < json.length; i++) {
-                if (json[i][_type] > max_value)
-                    max_value = json[i][_type]
+                // Create chart instance
+                var chart = am4core.create("chartdiv1", am4charts.XYChart);
+                chart.data = json
+                // chart.data = [{ "year": 2010.0, "농촌봉사": 656.0, "교육": 564.0, "노인": 39.0, "보건의료": 123.0, "문화": 115.0, "사무행정": 44.0, "국제협력": 55.0, "아동청소년": 136.0, "장애인": 225.0, "캠퍼스": 204.0, "환경": 264.0, "전체": 2425.0 }, { "year": 2011.0, "농촌봉사": 840.0, "교육": 489.0, "노인": 34.0, "보건의료": 119.0, "문화": 37.0, "사무행정": 157.0, "국제협력": 98.0, "아동청소년": 52.0, "장애인": 68.0, "캠퍼스": 167.0, "환경": 450.0, "전체": 2511.0 }, { "year": 2012.0, "농촌봉사": 184.0, "교육": 664.0, "노인": 36.0, "보건의료": 228.0, "문화": 129.0, "사무행정": 73.0, "국제협력": 159.0, "아동청소년": 120.0, "장애인": 76.0, "캠퍼스": 1366.0, "환경": 81.0, "전체": 3116.0 }, { "year": 2013.0, "농촌봉사": 929.0, "교육": 585.0, "노인": 68.0, "보건의료": 289.0, "문화": 131.0, "사무행정": 74.0, "국제협력": 221.0, "아동청소년": 174.0, "장애인": 66.0, "캠퍼스": 729.0, "환경": 31.0, "전체": 3297.0 }, { "year": 2014.0, "농촌봉사": 56.0, "교육": 455.0, "노인": 32.0, "보건의료": 173.0, "문화": 114.0, "사무행정": 48.0, "국제협력": 355.0, "아동청소년": 114.0, "장애인": 25.0, "캠퍼스": 361.0, "환경": 37.0, "전체": 1770.0 }, { "year": 2015.0, "농촌봉사": 382.0, "교육": 500.0, "노인": 49.0, "보건의료": 205.0, "문화": 63.0, "사무행정": 67.0, "국제협력": 259.0, "아동청소년": 48.0, "장애인": 35.0, "캠퍼스": 1436.0, "환경": 262.0, "전체": 3306.0 }, { "year": 2016.0, "농촌봉사": 170.0, "교육": 367.0, "노인": 146.0, "보건의료": 252.0, "문화": 129.0, "사무행정": 41.0, "국제협력": 121.0, "아동청소년": 248.0, "장애인": 192.0, "캠퍼스": 432.0, "환경": 129.0, "전체": 2227.0 }, { "year": 2017.0, "농촌봉사": 510.0, "교육": 563.0, "노인": 171.0, "보건의료": 224.0, "문화": 96.0, "사무행정": 30.0, "국제협력": 133.0, "아동청소년": 249.0, "장애인": 92.0, "캠퍼스": 897.0, "환경": 203.0, "전체": 3168.0 }, { "year": 2018.0, "농촌봉사": 206.0, "교육": 478.0, "노인": 97.0, "보건의료": 180.0, "문화": 127.0, "사무행정": 64.0, "국제협력": 183.0, "아동청소년": 342.0, "장애인": 101.0, "캠퍼스": 252.0, "환경": 114.0, "전체": 2144.0 }, { "year": 2019.0, "농촌봉사": 206.0, "교육": 478.0, "노인": 97.0, "보건의료": 180.0, "문화": 127.0, "사무행정": 64.0, "국제협력": 183.0, "아동청소년": 342.0, "장애인": 101.0, "캠퍼스": 252.0, "환경": 114.0, "전체": 2144.0 }]
 
-                if (json[i][_type] < min_value)
-                    min_value = json[i][_type]
-            }
+                var max_value = 0, min_value = 10000
+                for (var i = 0; i < json.length; i++) {
+                    if (json[i][_type] > max_value)
+                        max_value = json[i][_type]
 
-            // Create axes
-            //x-axis for chart
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-            categoryAxis.dataFields.category = "year";
-            categoryAxis.renderer.minGridDistance = 20;
-            // categoryAxis.title.text = "연도"
-            categoryAxis.renderer.grid.template.disabled = true;
-            // categoryAxis.dy = -20;
-            // categoryAxis.width = am4core.percent(90);
+                    if (json[i][_type] < min_value)
+                        min_value = json[i][_type]
+                }
 
-            //y-axis for chart
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            valueAxis.min = min_value - (max_value - min_value) * 0.1
-            valueAxis.renderer.inside = true;
-            // valueAxis.renderer.inversed = true;
-            valueAxis.renderer.grid.template.disabled = true;
-            valueAxis.renderer.baseGrid.disabled = true;
-            valueAxis.renderer.labels.template.disabled = true;
-            valueAxis.renderer.minGridDistance = 15;
+                // Create axes
+                //x-axis for chart
+                var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+                categoryAxis.dataFields.category = "year";
+                categoryAxis.renderer.minGridDistance = 20;
+                // categoryAxis.title.text = "연도"
+                categoryAxis.renderer.grid.template.disabled = true;
+                // categoryAxis.dy = -20;
+                // categoryAxis.width = am4core.percent(90);
 
-            // Create series
-            var series = chart.series.push(new am4charts.LineSeries());
-            series.dataFields.categoryX = "year"
-            series.dataFields.valueY = _type
-            series.strokeWidth = 4;
+                //y-axis for chart
+                var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+                valueAxis.min = min_value - (max_value - min_value) * 0.1
+                valueAxis.renderer.inside = true;
+                // valueAxis.renderer.inversed = true;
+                valueAxis.renderer.grid.template.disabled = true;
+                valueAxis.renderer.baseGrid.disabled = true;
+                valueAxis.renderer.labels.template.disabled = true;
+                valueAxis.renderer.minGridDistance = 15;
 
-            var bullet = series.bullets.push(new am4charts.CircleBullet());
-            bullet.circle.radius = 8;
-            /*
-            series.heatRules.push({
-                target: bullet.circle,
-                min: 5,
-                max: 20,
-                property: "radius"
-            });
-            */
+                // Create series
+                var series = chart.series.push(new am4charts.LineSeries());
+                series.dataFields.categoryX = "year"
+                series.dataFields.valueY = _type
+                series.strokeWidth = 4;
 
-            bullet.tooltipText = "{categoryX}학년도 " + _type + " 봉사활동 인원은 [bold]{valueY}명[/]입니다.";
+                var bullet = series.bullets.push(new am4charts.CircleBullet());
+                bullet.circle.radius = 8;
+                /*
+                series.heatRules.push({
+                    target: bullet.circle,
+                    min: 5,
+                    max: 20,
+                    property: "radius"
+                });
+                */
 
-            //scrollbars
-            /*
-            chart.scrollbarX = new am4core.Scrollbar();
-            chart.scrollbarY = new am4core.Scrollbar();
-            */
-        }); // end am4core.ready()
+                bullet.tooltipText = "{categoryX}학년도 " + _type + " 봉사활동 인원은 [bold]{valueY}명[/]입니다.";
 
-
+                //scrollbars
+                /*
+                chart.scrollbarX = new am4core.Scrollbar();
+                chart.scrollbarY = new am4core.Scrollbar();
+                */
+            }); // end am4core.ready()
+        }
         // ------------------------------------------------------------------------------------------------------------------------------------------------------
         // draw_circle
         // ------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -132,7 +133,9 @@ function draw_graph(_year, _type) {
 
         dataset["children"] = children_array;
         // console.log(dataset)
-        d3.select(self.frameElement).remove();
+        d3.select("#chartdiv2").select("svg").remove();
+
+
         var diameter = 900;
         var color = d3.scaleOrdinal(d3.schemeCategory20);
 
