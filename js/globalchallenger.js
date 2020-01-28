57d23fb3fb70e3b76358e472fc32b268126668f2
tkekdzhd <tkekdzhd@gmail.com>
2020-01-29 02:26:46 +0900

    동아리,글챌 그래프 수정

--- js/globalchallenger.js
+++ js/globalchallenger.js
@@ -1,4 +1,4 @@
-function Graph(json) {
+function lineGraph(json) {
     am4core.ready(function () {
         jQuery.getJSON("../../../json/globalchallenger.json", json => {
             // Themes begin
@@ -6,7 +6,53 @@ function Graph(json) {
             // Themes end
 
             // Create chart instance
-            var chart = am4core.create("chartdiv", am4charts.XYChart);
+            var chart = am4core.create("chartdiv1", am4charts.XYChart);
+
+            // Data for both series
+            var data = json;
+            console.log(json);
+
+            /* Create axes */
+            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+            categoryAxis.dataFields.category = "year";
+            categoryAxis.renderer.grid.template.location = 0;
+            categoryAxis.renderer.minGridDistance = 30;
+
+            /* Create value axis */
+            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.extraMax = 0.1;
+
+            /* Create series */
+            var lineSeries = chart.series.push(new am4charts.LineSeries());
+            lineSeries.name = "student";
+            lineSeries.dataFields.valueY = "student";
+            lineSeries.dataFields.categoryX = "year";
+
+            lineSeries.stroke = am4core.color("#fdd400");
+            lineSeries.strokeWidth = 3;
+            lineSeries.strokeDasharray = ["2,2"];
+            lineSeries.tooltip.label.textAlign = "middle";
+
+            var bullet = lineSeries.bullets.push(new am4charts.Bullet());
+            bullet.fill = am4core.color("#fdd400"); // tooltips grab fill from parent by default
+            bullet.tooltipText = "[#fff font-size: 15px]{categoryX}년:[#fff bold] {valueY}명[/][/]"
+            var circle = bullet.createChild(am4core.Circle);
+            circle.radius = 4;
+            circle.strokeWidth = 3;
+
+            chart.data = data;
+        });
+    }); // end am4core.ready()
+}
+function stackGraph(json) {
+    am4core.ready(function () {
+        jQuery.getJSON("../../../json/globalchallenger.json", json => {
+            // Themes begin
+            am4core.useTheme(am4themes_animated);
+            // Themes end
+
+            // Create chart instance
+            var chart = am4core.create("chartdiv2", am4charts.XYChart);
 
             // Data for both series
             var data = json;
@@ -28,35 +74,14 @@ function Graph(json) {
             columnSeries.dataFields.valueY = "team";
             columnSeries.dataFields.categoryX = "year";
 
-            columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{categoryX}년\n[/][#fff font-size: 20px]: {valueY}팀[/]"
+            columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{categoryX}년:[#fff bold] {valueY}팀[/][/]"
             columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
             columnSeries.columns.template.propertyFields.stroke = "stroke";
             columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
             columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
             columnSeries.tooltip.label.textAlign = "middle";
 
-            var lineSeries = chart.series.push(new am4charts.LineSeries());
-            lineSeries.name = "student";
-            lineSeries.dataFields.valueY = "student";
-            lineSeries.dataFields.categoryX = "year";
-
-            lineSeries.stroke = am4core.color("#fdd400");
-            lineSeries.strokeWidth = 3;
-            lineSeries.propertyFields.strokeDasharray = "lineDash";
-            lineSeries.tooltip.label.textAlign = "middle";
-
-            var bullet = lineSeries.bullets.push(new am4charts.Bullet());
-            bullet.fill = am4core.color("#fdd400"); // tooltips grab fill from parent by default
-            bullet.tooltipText = "[#fff font-size: 15px]{categoryX}년\n[/][#fff font-size: 20px]: {valueY}명[/]"
-            var circle = bullet.createChild(am4core.Circle);
-            circle.radius = 4;
-            circle.fill = am4core.color("#fff");
-            circle.strokeWidth = 3;
-
             chart.data = data;
         });
-
-
-
     }); // end am4core.ready()
 }
