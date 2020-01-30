c0485f43f107146ae165594fc251cfb6dde558a9
tkekdzhd <tkekdzhd@gmail.com>
2020-01-31 02:55:19 +0900

    산학협력연구부문기술이전

--- js/직급별직원수.js
+++ js/직급별직원수.js
@@ -166,53 +166,53 @@ parse2(json => {
     }); // end am4core.ready()}
 
 })
-function changegraph() {
-    am4core.ready(function () {
+// function changegraph() {
+//     am4core.ready(function () {
 
-        // Themes begin
-        am4core.useTheme(am4themes_animated);
-        // Themes end
+//         // Themes begin
+//         am4core.useTheme(am4themes_animated);
+//         // Themes end
 
-        // Create chart instance
-        var chart = am4core.create("chartdiv2", am4charts.PieChart);
+//         // Create chart instance
+//         var chart = am4core.create("chartdiv2", am4charts.PieChart);
 
-        // Add data
-        chart.data = json;
+//         // Add data
+//         chart.data = json;
 
-        var pieSeries = chart.series.push(new am4charts.PieSeries());
-        pieSeries.dataFields.value = "field";
-        pieSeries.dataFields.category = "name";
-        pieSeries.slices.template.stroke = am4core.color("#fff");
-        pieSeries.slices.template.strokeWidth = 2;
-        pieSeries.slices.template.tooltipText = "[bold]{category}[/]: {value.percent.formatNumber('#.0')}%({value}명)";
-        pieSeries.slices.template.strokeOpacity = 1;
-        pieSeries.colors.list = [
-            am4core.color("#dc67ce"),
-            am4core.color("#c767dc"),
-            am4core.color("#a367dc"),
-            am4core.color("#8067dc"),
-            am4core.color("#6771dc"),
-            am4core.color("#6794dc"),
-            am4core.color("#67b7dc"),
-        ];
+//         var pieSeries = chart.series.push(new am4charts.PieSeries());
+//         pieSeries.dataFields.value = "field";
+//         pieSeries.dataFields.category = "name";
+//         pieSeries.slices.template.stroke = am4core.color("#fff");
+//         pieSeries.slices.template.strokeWidth = 2;
+//         pieSeries.slices.template.tooltipText = "[bold]{category}[/]: {value.percent.formatNumber('#.0')}%({value}명)";
+//         pieSeries.slices.template.strokeOpacity = 1;
+//         pieSeries.colors.list = [
+//             am4core.color("#dc67ce"),
+//             am4core.color("#c767dc"),
+//             am4core.color("#a367dc"),
+//             am4core.color("#8067dc"),
+//             am4core.color("#6771dc"),
+//             am4core.color("#6794dc"),
+//             am4core.color("#67b7dc"),
+//         ];
 
-        // This creates initial animation
-        pieSeries.hiddenState.properties.opacity = 1;
-        pieSeries.hiddenState.properties.endAngle = -90;
-        pieSeries.hiddenState.properties.startAngle = -90;
+//         // This creates initial animation
+//         pieSeries.hiddenState.properties.opacity = 1;
+//         pieSeries.hiddenState.properties.endAngle = -90;
+//         pieSeries.hiddenState.properties.startAngle = -90;
 
-        pieSeries.slices.template.events.on("hit", function (ev) {
-            var series = ev.target.dataItem.component;
-            series.slices.each(function (item) {
-                if (item.isActive && item != ev.target) {
-                    item.isActive = false;
-                }
-            })
-        });
+//         pieSeries.slices.template.events.on("hit", function (ev) {
+//             var series = ev.target.dataItem.component;
+//             series.slices.each(function (item) {
+//                 if (item.isActive && item != ev.target) {
+//                     item.isActive = false;
+//                 }
+//             })
+//         });
 
-        // var grouper = pieSeries.plugins.push(new am4plugins_sliceGrouper.SliceGrouper());
-        // grouper.clickBehavior = "zoom";
-        // grouper.threshold = 15;
-    }); // end am4core.ready()}
+//         // var grouper = pieSeries.plugins.push(new am4plugins_sliceGrouper.SliceGrouper());
+//         // grouper.clickBehavior = "zoom";
+//         // grouper.threshold = 15;
+//     }); // end am4core.ready()}
 
-}
+// }
