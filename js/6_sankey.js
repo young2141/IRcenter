ed5ddad576f21e578244f18671c2a7643c31848a
min103jun <min103jun@naver.com>
2020-02-04 06:28:58 +0900

    v3

--- js/6_sankey.js
+++ js/6_sankey.js
@@ -1,123 +1,87 @@
+var color1 = {
+    "인문대학": "#FFD8D8",
+    "사회과학대학": "#F15F5F",
+    "자연과학대학": "#FAE0D4",
+    "경상대학": "#F29661",
+    "공과대학": "#FAECC5",
+    "IT대학": "#F2CB61",
+    "농업생명과학대학": "#FAF4C0",
+    "예술대학": "#E5D85C",
+    "사범대학": "#34F7BA",
+    "의과대학": "#BCE55C",
+    "치과대학": "#CEF279",
+    "수의과대학": "#9FC93C",
+    "생활과학대학": "#B7F0B1",
+    "간호대학": "#47C8CE",
+    "약학대학": "#B2EBF4",
+    "글로벌인재학부": "#5CD1E5",
+    "행정학부": "#D9E5FF",
+    "법과대학": "#6699FF",
+    "자율전공부": "#DAD9FF",
+    "No Degree": "#6B66FF"
+};
 function parsing() {
     var filename = "doyeong_sankey_dumy.json";
-    var color1 = {
-        "인문대학" : "#99ff6a",
-        "사회과학대학" : "#ff0000",
-        "자연과학대학" : "#00ff00",
-        "경상대학" : "#0000ff",
-        "공과대학" : "#ffff00",
-        "IT대학" : "#ff00ff",
-        "농업생명과학대학" : "#00ffff",
-        "예술대학" : "#f0000f",
-        "사범대학" : "#f00f00",
-        "의과대학" : "#f000f0",
-        "치과대학" : "#f0f0f0",
-        "수의과대학" : "#f0f0ff",
-        "생활과학대학" : "#f00fff",
-        "간호대학" : "#f0ffff",
-        "약학대학" : "#ffff0f",
-        "글로벌인재학부" : "#f00fff",
-        "행정학부" : "#123456",
-        "법과대학" : "#66ffff",
-        "자율전공부" : "#29845a",
-        "No Degree" : "#0f0f0f"
-    };
-    var color2 = {
-        "인문대학" : "#99ff6b",
-        "사회과학대학" : "#ff0001",
-        "자연과학대학" : "#00ff01",
-        "경상대학" : "#0000fe",
-        "공과대학" : "#ffff01",
-        "IT대학" : "#ff00fe",
-        "농업생명과학대학" : "#00fffe",
-        "예술대학" : "#f0000e",
-        "사범대학" : "#f00f01",
-        "의과대학" : "#f000f1",
-        "치과대학" : "#f0f0f1",
-        "수의과대학" : "#f0f0fe",
-        "생활과학대학" : "#f00ffe",
-        "간호대학" : "#f0fffe",
-        "약학대학" : "#ffff0e",
-        "글로벌인재학부" : "#f00ffe",
-        "행정학부" : "#123455",
-        "법과대학" : "#66fffe",
-        "자율전공부" : "#29845b",
-        "No Degree" : "#0f0f0e"
-    }
+
     $.getJSON("../../../json/" + filename, (jsonData) => {
-        var keys = Object.keys(color1);
-        var pre = "";
         var len = jsonData.length;
 
-        for(var i=0; i<len; i++){
-            if(jsonData[i].category == "입학후전입")    
-                jsonData[i].color = color1[(jsonData[i].from).trim()];
-            else{
-                if(pre == jsonData[i].from.trim()) continue
-                pre = jsonData[i].from.trim();
-                jsonData.push({
-                    "from" : jsonData[i].from,
-                    "color" : color1[jsonData[i].from.trim()]
-                });
-
-                jsonData.push({
-                    "to" : jsonData[i].to,
-                    "color" : color1[jsonData[i].to.trim()]
-                });
-            }            
+        for (var i = 0; i < len; i++) {
+            jsonData[i].color = color1[(jsonData[i].from).trim()];
         }
+
         drawSankey(jsonData);
     })
 }
 
 function drawSankey(_data) {
     am4core.ready(function () {
-
         // Themes begin
         am4core.useTheme(am4themes_animated);
         // Themes end
-    
+
         var chart = am4core.create("chartdiv", am4charts.SankeyDiagram);
         chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
-    
+
         chart.data = _data;
-    
+
         let hoverState = chart.links.template.states.create("hover");
         hoverState.properties.fillOpacity = 0.6;
-    
+
         chart.dataFields.fromName = "from";
         chart.dataFields.toName = "to";
         chart.dataFields.value = "value";
-        chart.dataFields.color = "color";
-    
+        //chart.dataFields.color = "color";
+
+        console.log(chart.dataItem);
         //chart.links.template.propertyFields.id = "id";
         chart.links.template.colorMode = "solid";
         chart.links.template.fill = new am4core.InterfaceColorSet().getFor("alternativeBackground");
         chart.links.template.fillOpacity = 0.1;
-        chart.links.template.tooltipText = "";
-    
+        chart.links.template.tooltipText = "{fromName} -> {toName} : {value}";
+
         // highlight all links with the same id beginning
         // chart.links.template.events.on("over", function (event) {
         //     let link = event.target;
         //     let id = link.id.split("-")[0];
         //     console.log("아이디값 : " + id);
-    
+
         //     chart.links.each(function (link) {
         //         if (link.id.indexOf(id) != -1) {
         //             link.isHover = false;
         //         }
         //     })
         // })
-    
+
         // chart.links.template.events.on("out", function (event) {
         //     chart.links.each(function (link) {
         //         link.isHover = false;
         //     })
         // })
-    
+
         // for right-most label to fit
         chart.paddingRight = 150;
-    
+
         // make nodes draggable
         var nodeTemplate = chart.nodes.template;
         nodeTemplate.inert = true;
@@ -125,16 +89,16 @@ function drawSankey(_data) {
         nodeTemplate.showSystemTooltip = true;
         nodeTemplate.width = 20;
 
-        // make nodes draggable
-        var nodeTemplate = chart.nodes.template;
         nodeTemplate.readerTitle = "Click to show/hide or drag to rearrange";
         nodeTemplate.showSystemTooltip = true;
         nodeTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer
 
+
         nodeTemplate.adapter.add("fill", function (fill, target) {
-            console.log(target.dataItem);
-            return target.dataItem.color;
-        })
+            clr = color1[target.dataItem.fromName.trim()];
+            return am4core.color(clr);
+        });
+
     }); // end am4core.ready()
 }
 
