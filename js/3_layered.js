1febf09971a064b193a194568b4311e2b575ad4a
min103jun <min103jun@naver.com>
2020-02-04 03:38:41 +0900

    ver2

--- js/3_layered.js
+++ js/3_layered.js
@@ -1,39 +1,16 @@
 var year;
-function conditions() {
-    year = document.getElementById("years").value;
-    var cond = [
-        "학생부교과",
-        "논술",
-        "학생부종합 일반",
-        "학생부종합 농어촌",
-        "정시 일반"
-    ];
-    for (var i = 0; i < 5; i++) {
-        parsing2(cond[i], i + 1);
-    }
-}
 
-function parsing2(_condition, _cnt) {
-    var filename = "3page_" + _condition + ".json";
-    $.getJSON("../../json/" + filename, jsonData => {
-        var data = [];
-        var categories = ["1~1.5", "1.5~2", "2~2.5", "2.5~3.5", "3.5~5"];
-        categories.reverse();
-        for (var i = 0; i < 5; i++) {
-            var temp = {};
-            temp["category"] = categories[i] + "등급";
-            temp["apply"] = jsonData["apply"][categories[i]];
-            temp["admission"] = jsonData["admission"][categories[i]];
-            if (jsonData["pass"]) temp["pass"] = jsonData["pass"][categories[i]];
-            data.push(temp);
-        }
-        drawLayeredChart(data, _cnt, _condition);
+function parsing2(_condition) {
+    $.getJSON("../../json/3page_layerd_data.json", jsonData => {
+        drawLayeredChart(jsonData);
     });
 }
 
-function drawLayeredChart(_data, _cnt, _condition) {
-    var condition = ["학생부교과", "논술", "학생부종합 일반", "학생부종합 농어촌", "정시 일반"]
-    am4core.useTheme(am4themes_animated);
+function drawLayeredChart(_data) {
+    var typename = ["student", "nonsul", "normal", "farmfish", "jeongsi"];
+    var typename_kr = ["학생부교과", "논술", "학생부종합 일반", "학생부종합 농어촌", "정시 일반"];
+    var year = document.getElementById("years").value;
+    //am4core.useTheme(am4themes_animated);
 
     var container = am4core.create("chartdiv2", am4core.Container);
     container.layout = "grid";
@@ -41,26 +18,28 @@ function drawLayeredChart(_data, _cnt, _condition) {
     container.width = am4core.percent(100);
     container.height = am4core.percent(100);
 
-    function layerChart(cond) {
+    function layerChart(_data, cond) {
         var chart = container.createChild(am4charts.XYChart);
-        chart.width = am4core.percent(15);
+        chart.width = am4core.percent(20);
 
         chart.data = _data;
-
+        console.log(_data);
         var title = chart.titles.create();
         title.text = cond;
         title.fontSize = 17;
-        title.marginBottom = 10;
-        title.paddingLeft = 50;
+        title.dy = -10;
+        title.textAlign = "middle";
 
         // Create axes : 가로축
         var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "category";
+        categoryAxis.dataFields.category = "grade";
+        categoryAxis.renderer.inversed = true;
         categoryAxis.renderer.grid.template.location = 0;
         categoryAxis.renderer.minGridDistance = 30;
 
         // 세로축
         var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.labels.template.disabled = true; //y축 space를 0으로 만듬
         valueAxis.max = 7000;
         valueAxis.min = 0;
 
@@ -71,23 +50,24 @@ function drawLayeredChart(_data, _cnt, _condition) {
         function createSeries(_chart, value, ko, percent, rgb) {
             var series = _chart.series.push(new am4charts.ColumnSeries());
             series.dataFields.valueY = value; // 총 지원자 수
-            series.dataFields.categoryX = "category";
+            series.dataFields.categoryX = "grade";
 
             series.clustered = false;
  
             series.columns.template.stroke = am4core.color(rgb); // color
             series.columns.template.fill = am4core.color(rgb); // color
+            //series.tooltip.getFillFromObject = false;
             if (ko == "지원인원") {
-                series.columns.template.width = am4core.percent(85);
+                series.columns.template.width = am4core.percent(80);
                 series.columns.template.tooltipText = year + "학년도 " + cond + "전형 지원자는 {valueY}명 입니다.";
             }
             else if (ko == "기준점수 통과인원") {
-                series.columns.template.width = am4core.percent(40);
-                series.columns.template.tooltipText = year + "학년도 " + cond + "전형 지원자는 {valueY}명 입니다.";
+                series.columns.template.width = am4core.percent(60);
+                series.columns.template.tooltipText = year + "학년도 " + cond + "전형 최저기준 통과자는 {valueY}명 입니다.";
             }
             else {
-                series.columns.template.width = am4core.percent(20);
-                series.columns.template.tooltipText = year + "학년도 " + cond + "전형 지원자는 {valueY}명 입니다.";
+                series.columns.template.width = am4core.percent(40);
+                series.columns.template.tooltipText = year + "학년도 " + cond + "전형 입학자는 {valueY}명 입니다.";
             }
 
         }
@@ -101,6 +81,6 @@ function drawLayeredChart(_data, _cnt, _condition) {
     }
 
     for (var i = 0; i < 5; i++) {
-        layerChart(condition[i]);
+        layerChart(_data[typename[i]], typename_kr[i]);
     }
 }
