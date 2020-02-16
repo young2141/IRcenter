46f27cda40bea3acc482c9e87aa2b39f38d3a2c9
TashaKim <51108729+TashaKim-MJ@users.noreply.github.com>
2020-02-16 20:13:00 +0900

    Add files via upload

--- working on/js/table_학생현황.js
+++ working on/js/table_학생현황.js
@@ -1,8 +1,8 @@
 function years() {
     var start = 1946;
     var end = 2019;
-    var condition1 = $("select[name=grad]").val();
-    var condition2 = $("select[name=gender]").val();
+    var condition1 = $("select[id=grad]").val();
+    var condition2 = $("select[id=gender]").val();
     console.log(condition1, condition2);
     var data = [];
     var depth = 4;
@@ -12,23 +12,43 @@ function years() {
   function parsing1(_condition1, _condition2, _data, _depth, _start, _end) {
     var filename = "area_chart_data_v2.json";
     var temp = {};
-    $.getJSON("../../json/" + filename, jsonData => {
+    $.getJSON("../../../json/" + filename, jsonData => {
       for (var i = 0; i < jsonData.length; i++) 
       {
+        console.log(jsonData[i]["year"]);
         temp["year"] = jsonData[i]["year"];
-        if (_condition2 == all){
+        if (_condition1 == "all"){
+          if (_condition2 == "all"){
+            temp["college"] = jsonData[i]["graduate_college"] + jsonData[i]["undergraduate_college"];
+            temp["master"] = jsonData[i]["graduate_master"] + jsonData[i]["undergraduate_master"];
+            temp["doctor"] = jsonData[i]["graduate_doctor"] + jsonData[i]["undergraduate_doctor"];
+          }
+          else{
+            temp["college"] = jsonData[i]["graduate_" + _condition2 + "_college"] + jsonData[i]["undergraduate_" + _condition2 + "_college"];
+            temp["master"] = jsonData[i]["graduate_" + _condition2 + "_master"] + jsonData[i]["undergraduate_" + _condition2 + "_master"];
+            temp["doctor"] = jsonData[i]["graduate_" + _condition2 + "_doctor"] + jsonData[i]["undergraduate_" + _condition2 + "_doctor"];
+          }
+        }
+        else{
+          if (_condition2 == "all"){
             temp["college"] = jsonData[i][_condition1 + "_college"];
             temp["master"] = jsonData[i][_condition1 + "_master"];
             temp["doctor"] = jsonData[i][_condition1 + "_doctor"];
-        }
-        else{
+          }
+          else{
             temp["college"] = jsonData[i][_condition1 + "_" + _condition2 + "_college"];
             temp["master"] = jsonData[i][_condition1 + "_" + _condition2 + "_master"];
             temp["doctor"] = jsonData[i][_condition1 + "_" + _condition2 + "_doctor"];
+          }
         }
+        console.log(temp);
         _data.push(temp);
+        if(i == 73){
+          console.log(_data);
+          drawTable(_data, _depth, _start, _end);
+        }
       }
-        drawTable(_data, _depth, _start, _end);
+        // drawTable(_data, _depth, _start, _end);
     });
   }
 
