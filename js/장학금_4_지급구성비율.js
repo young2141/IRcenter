29b767ed9cff33d1ff087357bfeb01ebea602c3a
imjspk <59077504+imjspk@users.noreply.github.com>
2020-03-20 09:32:39 +0900

    장학금_2차

--- js/장학금_4_지급구성비율.js
+++ js/장학금_4_지급구성비율.js
@@ -1,61 +1,55 @@
-﻿
-var jdata = []
+﻿var jdata = []
+var type = [["성적우수", "저소득층", "근로", "교직원", "기타", "국가", "지방자치단체", "사설 및 기타"],
+    ["성적우수", "저소득층", "근로", "교직원", "기타"],
+    ["국가", "지방자치단체", "사설 및 기타"]]
+var scholarship = ["전체 장학금", "교내 장학금", "교외 장학금"]
 
-function parse(callback) {
-    $.getJSON("../../../json/research.json", json => {
-        callback(json);
+function parse1(callback) {
+    $.getJSON("../../../json/전공분류.json", json1 => {
+        callback(json1);
     });
 }
 
-function check_data(json) {
+function parse2(callback, path) {
+    $.getJSON("../../../json/" + path, json2 => {
+        callback(json2);
+    });
+}
+
+function check_data(json, major_dict, sort, year, affiliation) {
     var result = []
+    var result_temp = {}
     var obj_temp = {}
-    var majors = Object.keys(json[0]["전임교원_남자"])
+    var keys = []
+    var keys2 = []
+
+    for (var i = 0; i < type[sort].length; i++) {
+        result_temp[type[sort][i]] = 0
+    }
 
     for (var i = 0; i < json.length; i++) {
+        if (json[i]["year"] != year) continue
+
+        keys = Object.keys(json[i])
+
+        for (var j = 0; j < keys.length; j++) {
+            if (keys[j] == "year" || (major_dict[keys[j]] != affiliation + "계열" && affiliation != "(전체)")) continue
+
+            keys2 = Object.keys(json[i][keys[j]])
+            for (var k = 0; k < keys2.length; k++) {
+                if (keys2[k] != "재학생" && keys2[k] != "수혜인원") {
+                    if ($.inArray(keys2[k], type[sort]) != -1)
+                        result_temp[keys2[k]] += json[i][keys[j]][keys2[k]]
+                }
+            }
+        }
+    }
+
+    for (var i = 0; i < type[sort].length; i++) {
         obj_temp = {
-            "year": json[i]["year"],
-            "논문_전체": 0,
-            "논문_국내_연구재단": 0,
-            "논문_국내_기타": 0,
-            "논문_국제_SCI": 0,
-            "논문_국제_기타": 0,
-            "저역서_전체": 0,
-            "저역서_저서": 0,
-            "저역서_역서": 0
+            "category": type[sort][i],
+            "value": result_temp[type[sort][i]]
         }
-
-        for (var j = 0; j < majors.length; j++) {
-            if (json[i]["국내_연구재단_남자"][majors[j]] != "" && json[i]["국내_연구재단_남자"][majors[j]] != 0)
-                obj_temp["논문_국내_연구재단"] += json[i]["국내_연구재단_남자"][majors[j]]
-            if (json[i]["국내_연구재단_여자"][majors[j]] != "" && json[i]["국내_연구재단_여자"][majors[j]] != 0)
-                obj_temp["논문_국내_연구재단"] += json[i]["국내_연구재단_여자"][majors[j]]
-            if (json[i]["국내_기타_남자"][majors[j]] != "" && json[i]["국내_기타_남자"][majors[j]] != 0)
-                obj_temp["논문_국내_기타"] += json[i]["국내_기타_남자"][majors[j]]
-            if (json[i]["국내_기타_여자"][majors[j]] != "" && json[i]["국내_기타_여자"][majors[j]] != 0)
-                obj_temp["논문_국내_기타"] += json[i]["국내_기타_여자"][majors[j]]
-
-            if (json[i]["국제_SCI_남자"][majors[j]] != "" && json[i]["국제_SCI_남자"][majors[j]] != 0)
-                obj_temp["논문_국제_SCI"] += json[i]["국제_SCI_남자"][majors[j]]
-            if (json[i]["국제_SCI_여자"][majors[j]] != "" && json[i]["국제_SCI_여자"][majors[j]] != 0)
-                obj_temp["논문_국제_SCI"] += json[i]["국제_SCI_여자"][majors[j]]
-            if (json[i]["국제_기타_남자"][majors[j]] != "" && json[i]["국제_기타_남자"][majors[j]] != 0)
-                obj_temp["논문_국제_기타"] += json[i]["국제_기타_남자"][majors[j]]
-            if (json[i]["국제_기타_여자"][majors[j]] != "" && json[i]["국제_기타_여자"][majors[j]] != 0)
-                obj_temp["논문_국제_기타"] += json[i]["국제_기타_여자"][majors[j]]
-
-            if (json[i]["저서_남자"][majors[j]] != "" && json[i]["저서_남자"][majors[j]] != 0)
-                obj_temp["저역서_저서"] += json[i]["저서_남자"][majors[j]]
-            if (json[i]["저서_여자"][majors[j]] != "" && json[i]["저서_여자"][majors[j]] != 0)
-                obj_temp["저역서_저서"] += json[i]["저서_여자"][majors[j]]
-            if (json[i]["역서_남자"][majors[j]] != "" && json[i]["역서_남자"][majors[j]] != 0)
-                obj_temp["저역서_역서"] += json[i]["역서_남자"][majors[j]]
-            if (json[i]["역서_여자"][majors[j]] != "" & json[i]["역서_여자"][majors[j]] != 0)
-                obj_temp["저역서_역서"] += json[i]["역서_여자"][majors[j]]
-        }
-
-        obj_temp["논문_전체"] = obj_temp.논문_국내_연구재단 + obj_temp.논문_국내_기타 + obj_temp.논문_국제_SCI + obj_temp.논문_국제_기타
-        obj_temp["저역서_전체"] = obj_temp.저역서_저서 + obj_temp.저역서_역서
 
         result.push(obj_temp)
     }
@@ -63,94 +57,52 @@ function check_data(json) {
     return result
 }
 
-function draw_graph(_sort1) {
-    parse(json => {
-        if (jdata.length == 0) {
-            jdata = check_data(json)
-        }
+function draw_graph(_sort1, _year, _sem, _aff) {
+    parse1(json1=> {
 
-        am4core.ready(function () {
-            // Themes begin
-            am4core.useTheme(am4themes_animated);
-            // Themes end
+        parse2(json2 => {
 
-            var _gpindex = (_sort1 == "논문" ? 1 : 2)
-            // Create chart instance
-            var chart = am4core.create("chartdiv" + _gpindex, am4charts.XYChart);
+            jdata = check_data(json2, json1, _sort1 - 1, _year, _aff)
 
-            // Add data
-            chart.data = jdata
+            am4core.ready(function () {
+                if (_sort1 == 1) am4core.disposeAllCharts();
 
-            console.log(JSON.stringify(chart.data))
+                // Themes begin
+                am4core.useTheme(am4themes_animated);
+                // Themes end
 
-            // Create axes
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-            categoryAxis.dataFields.category = "year";
-            categoryAxis.renderer.grid.template.location = 0;
-            categoryAxis.renderer.minGridDistance = 30;
+                // Create chart instance
+                var chart = am4core.create("chartdiv" + _sort1, am4charts.PieChart);
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            // valueAxis.renderer.inside = true;
-            // valueAxis.renderer.labels.template.disabled = true;
-            valueAxis.min = 0;
-            valueAxis.extraMax = 0.15;
-            // valueAxis.extraMin = 0.15;
+                // Add data
+                chart.data = jdata
+                
+                
+                console.log(JSON.stringify(chart.data))
 
-            var span_legend = document.getElementsByName("span_legend_" + _sort1)
-            // console.log(span_legend_all.id)
-            var index = 0
+                var pieSeries = chart.series.push(new am4charts.PieSeries());
+                if (_sort1 != 1) {
+                    chart.radius = am4core.percent(40)
+                    pieSeries.labels.template.fontSize = 14;
+                }
+                pieSeries.dataFields.value = "value";
+                pieSeries.dataFields.category = "category";
+                pieSeries.slices.template.strokeWidth = 2;
+                pieSeries.slices.template.strokeOpacity = 1;
+                pieSeries.slices.template.stroke = am4core.color("#ffffff");
+                pieSeries.tooltip.getFillFromObject = false;
+                pieSeries.slices.template.tooltipText =
+                  "[#000]" + _year  + "학년도 " + _sem[0] + "학기 " + scholarship[_sort1 - 1] + " 중 {category} 장학금은 [bold]{value.percent.formatNumber('#.#')}%[] 입니다.";
+                // 툴팁에 실제 value값도 필요하다면 ({value}명) 추가하기
+                pieSeries.tooltip.label.fill = am4core.color("#000000");
+                // This creates initial animation
+                pieSeries.hiddenState.properties.opacity = 1;
+                pieSeries.hiddenState.properties.endAngle = -90;
+                pieSeries.hiddenState.properties.startAngle = -90;
 
-            // Create series
-            function createSeries(name, message) {
-                var series = chart.series.push(new am4charts.LineSeries());
-                series.name = name
-                series.dataFields.categoryX = "year"
-                series.dataFields.valueY = name
-                series.strokeWidth = 3;
 
-                series.sequencedInterpolation = true;
 
-                var bullet = series.bullets.push(new am4charts.CircleBullet());
-                bullet.circle.radius = 4;
-                bullet.circle.strokeWidth = 3;
-                bullet.tooltipText = "{categoryX}학년도 " + message + "은 [bold]{valueY}" + (_sort1 == "논문" ? "편" : "권") + "[/]입니다.";
-
-                series.strokeDasharray = ["dotted"];
-                if (name.indexOf("전체") == -1)
-                    series.strokeDasharray = "2, 2";
-
-                series.stroke = am4core.color(span_legend[index].style.color)
-                bullet.circle.stroke = am4core.color(span_legend[index].style.color)
-                bullet.circle.fill = am4core.color(span_legend[index].style.color)
-                bullet.fill = am4core.color(span_legend[index++].style.color)
-
-                /*
-                series.stroke = am4core.color(span_legend_all.style.color)
-                bullet.circle.stroke = am4core.color(span_legend_all.style.color)
-                bullet.circle.fill = am4core.color(span_legend_all.style.color)
-                bullet.fill = am4core.color(span_legend_all.style.color)
-                */
-            }
-
-            if (_sort1 == "논문") {
-                createSeries("논문_전체", "논문 전체 실적")
-                createSeries("논문_국내_연구재단", "국내 연구재단 등재지(후보포함) 실적")
-                createSeries("논문_국내_기타", "국내 기타 학술지 실적")
-                createSeries("논문_국제_SCI", "SCI급 및 SCOPUS 학술지 실적")
-                createSeries("논문_국제_기타", "국제 기타 학술지 실적")
-            }
-            else {
-                createSeries("저역서_전체", "저·역서 전체 실적")
-                createSeries("저역서_저서", "저서 실적")
-                createSeries("저역서_역서", "역서 실적")
-            }
-
-            // Legend
-            // chart.legend = new am4charts.Legend();
-
-        }); // end am4core.ready()
+            }); // end am4core.ready()
+        }, "scholarship_" + _sem + ".json")
     })
 }
-
-draw_graph("논문")
-draw_graph("저·역서")
